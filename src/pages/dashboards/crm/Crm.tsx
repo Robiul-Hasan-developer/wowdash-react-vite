fd9406f03d56247ec6e1bb7c2b8c6cd66e30ad0b
Robiul Hasan <robiulhasan9559@gmail.com>
2025-12-08 10:44:51 +0600

    (Component) Revenue card & chart added

--- src/pages/dashboards/crm/Crm.tsx
+++ src/pages/dashboards/crm/Crm.tsx
@@ -1,15 +1,62 @@
-import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
+import Breadcrumb from "@/layouts/Breadcrumb";
+import RevenueGrowthCard from "./components/RevenueGrowthCard";
 
-const Crm = () => {
+const AiDashboard = () => {
     return (
-        <div>
-            <h1 className="mb-6">CRM dashboard Page</h1>
-                <Button asChild>
-                    <Link to="/">Go to AI Page</Link>
-                </Button>
-        </div>
+        <>
+            <Breadcrumb title="CRM" text="CRM" />
+
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 mt-6">
+
+                <div className="lg:col-span-12 2xl:col-span-8">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-6">
+                        {/* <StatsCard /> */}
+                    </div>
+                </div>
+
+                <div className="lg:col-span-12 2xl:col-span-4">
+                    <RevenueGrowthCard />
+                </div>
+
+                <div className="lg:col-span-12 2xl:col-span-8">
+                    {/* <EarningStatisticsCard /> */}
+                </div>
+
+                <div className="lg:col-span-12 2xl:col-span-4">
+                    <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                        <div className="lg:col-span-6 2xl:col-span-12 col-xxl-12 col-sm-6">
+                            {/* <CampaignCard /> */}
+                        </div>
+                        <div className="lg:col-span-6 2xl:col-span-12 col-xxl-12 col-sm-6">
+                            {/* <CustomerOverviewCard /> */}
+                        </div>
+                    </div>
+                </div>
+
+                <div className="lg:col-span-6 2xl:col-span-4">
+                    {/* <ClientPaymentStatusCard /> */}
+                </div>
+
+                <div className="lg:col-span-6 2xl:col-span-4">
+                    {/* <CountriesStatusCard /> */}
+                </div>
+
+                <div className="lg:col-span-12 2xl:col-span-4">
+                    {/* <TopPerformerCard /> */}
+                </div>
+
+                <div className="lg:col-span-12 2xl:col-span-6">
+                    {/* <TodoListRecentCard /> */}
+                </div>
+
+                <div className="lg:col-span-12 2xl:col-span-6">
+                    {/* <LastTransactionCard /> */}
+                </div>
+
+            </div>
+
+        </>
     );
 };
 
-export default Crm;
+export default AiDashboard;
