fadd598b49e5e0768dc297fc7fdfd40a22177539
Robiul Hasan <robiulhasan9559@gmail.com>
2025-12-10 18:21:08 +0600

    (Page) LMS page initial setup done

---
+++ src/pages/dashboards/lms/Lms.tsx
@@ -0,0 +1,40 @@
+import Breadcrumb from "@/layouts/Breadcrumb";
+import LoadingSkeleton from "@/loading/LoadingSkeleton";
+import { Suspense } from "react";
+
+const Lms = () => {
+    return (
+        <>
+            <Breadcrumb title="LMS" text="LMS" />
+
+             <div className="grid grid-cols-1 2xl:grid-cols-12 gap-6">
+                <div className="col-span-12 2xl:col-span-8">
+                    <Suspense fallback={<LoadingSkeleton height="h-64" text="Loading..." />}>
+                        {/* <WidgetsAverageChart /> */}
+                    </Suspense>
+                </div>
+                <div className="col-span-12 md:col-span-6 2xl:col-span-4">
+                    {/* <TrafficSourceCard /> */}
+                </div>
+                <div className="col-span-12 md:col-span-6 2xl:col-span-4">
+                    {/* <TopCategoriesCard /> */}
+                </div>
+                <div className="col-span-12 md:col-span-6 2xl:col-span-4">
+                    {/* <TopInstructorsCard /> */}
+                </div>
+                <div className="col-span-12 md:col-span-6 2xl:col-span-4">
+                    {/* <StudentProgressCard /> */}
+                </div>
+                <div className="col-span-12 2xl:col-span-8">
+                    {/* <CoursesCard /> */}
+                </div>
+                <div className="col-span-12 2xl:col-span-4">
+                    {/* <CourseActivityCard /> */}
+                </div>
+            </div>
+
+        </>
+    );
+};
+
+export default Lms;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Cryptocurrency from "@/pages/dashboards/cryptocurrency/Cryptocurrency";
 import Investment from "@/pages/dashboards/investment/Investment";
+import Lms from "@/pages/dashboards/lms/lms";
 import NotFound from '../error/404';
 import ErrorBoundary from "../error/ErrorBoundary";
 import MainLayout from "../layouts/MainLayout";
@@ -29,6 +30,9 @@ export const router = createBrowserRouter([
       },
       {
         path: "investment", element: <Investment />
+      },
+      {
+        path: "lms", element: <Lms />
       },
     ],
   },
