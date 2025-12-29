9a8a39c4dd4806dfbfb4b6cb2b6b7fb6327a2aa1
Robiul Hasan <robiulhasan9559@gmail.com>
2025-12-29 16:03:58 +0600

    (fix) handle Profile Update ts file updated

--- src/pages/users/view-profile/components/actions/handleProfileUpdate.ts
+++ src/pages/users/view-profile/components/actions/handleProfileUpdate.ts
@@ -1,11 +1,9 @@
-'use server';
-
 export async function handleProfileUpdate(formData: FormData) {
-  const name = formData.get('name');
-  const email = formData.get('email');
-  const phone = formData.get('number');
-  const department = formData.get('department');
-  const designation = formData.get('designation');
-  const language = formData.get('language');
-  const description = formData.get('desc');
+  formData.get("name");
+  formData.get("email");
+  formData.get("number");
+  formData.get("department");
+  formData.get("designation");
+  formData.get("language");
+  formData.get("desc");
 }
