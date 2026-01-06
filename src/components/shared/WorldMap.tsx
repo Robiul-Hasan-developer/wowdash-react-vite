1e318051614f2632eb0696768a7f922d2e8a58c0
Robiul Hasan <robiulhasan9559@gmail.com>
2026-01-06 10:34:48 +0600

    (Fix) World map udpated

--- src/components/shared/WorldMap.tsx
+++ src/components/shared/WorldMap.tsx
@@ -4,8 +4,8 @@ import WorldMapImage from '@/assets/world-map/world-map.png';
 const WorldMap = () => {
 
   return (
-    <div className={`h-full relative`}>
-        <img src={WorldMapImage} alt="World Map" className='w-full h-full object-cover' />
+    <div className={`h-full relative w-full flex justify-center items-center`}>
+        <img src={WorldMapImage} alt="World Map" className='' />
     </div>
   );
 };
