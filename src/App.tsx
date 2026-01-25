9b3e6a69bc099a528c63470f6a8f370ff133d9cf
Robiul Hasan <robiulhasan9559@gmail.com>
2026-01-25 19:48:13 +0600

    (Issues Fix) App file done

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,59 @@
+// import { ThemeProvider } from "@/components/theme-provider";
+// import { useEffect, useState } from 'react';
+// import { RouterProvider } from 'react-router-dom';
+// import { IsSubmittingContextProvider } from './context/isSubmittingContext';
+// import { LoadingProvider } from './context/LoadingContext';
+// import { PageLoader } from './loading/PageLoader';
+// import { router } from './routes/AppRoutes';
+
+// function App() {
+
+//   const [loading, setLoading] = useState(true);
+//   useEffect(() => {
+//     const timer = setTimeout(() => setLoading(false));
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   if (loading) return <PageLoader />;
+
+//   return (
+//     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+//       <LoadingProvider>
+//         <IsSubmittingContextProvider>
+//           <RouterProvider router={router} />
+//         </IsSubmittingContextProvider>
+//       </LoadingProvider>
+//     </ThemeProvider>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
 import { ThemeProvider } from "@/components/theme-provider";
-import { useEffect, useState } from 'react';
-import { RouterProvider } from 'react-router-dom';
-import { IsSubmittingContextProvider } from './context/isSubmittingContext';
-import { LoadingProvider } from './context/LoadingContext';
-import { PageLoader } from './loading/PageLoader';
-import { router } from './routes/AppRoutes';
+import { RouterProvider } from "react-router-dom";
+import { EmailSidebarProvider } from "./context/EmailSidebarContext";
+import { LoadingProvider } from "./context/LoadingContext";
+import { IsSubmittingContextProvider } from "./context/isSubmittingContext";
+import { router } from "./routes/AppRoutes";
 
 function App() {
-
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    const timer = setTimeout(() => setLoading(false));
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (loading) return <PageLoader />;
-
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <LoadingProvider>
         <IsSubmittingContextProvider>
-          <RouterProvider router={router} />
+          <EmailSidebarProvider>
+            <RouterProvider router={router} />
+          </EmailSidebarProvider>
         </IsSubmittingContextProvider>
       </LoadingProvider>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
