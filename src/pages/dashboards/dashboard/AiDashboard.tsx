6ee265ef29ad033cde95992638ce4288ae7608cf
Robiul Hasan <robiulhasan9559@gmail.com>
2026-01-26 10:43:15 +0600

    (Issues Fix) Social login previous code aded

--- src/pages/dashboards/dashboard/AiDashboard.tsx
+++ src/pages/dashboards/dashboard/AiDashboard.tsx
@@ -15,6 +15,12 @@ const AiDashboard = () => {
         <>
             <Breadcrumb title="AI" text="AI" />
 
+            {/* <ErrorBoundary fallback={<ErrorBoundary />}>
+                <Suspense fallback={<LoadingSkeleton />}>
+                 <StatCard />
+                </Suspense>
+            </ErrorBoundary> */}
+
             <Suspense fallback={<LoadingSkeleton />}>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 gap-6">
                     <StatCard />
