3932a3b972b851621c13bee7581d667df24f8900
Robiul Hasan <robiulhasan9559@gmail.com>
2026-03-12 13:53:44 +0600

    (Component) Total User & total podcast component done

--- src/pages/dashboards/podcast/Podcast.tsx
+++ src/pages/dashboards/podcast/Podcast.tsx
@@ -1,16 +1,15 @@
 import LazyWrapper from "@/components/LazyWrapper";
 import Breadcrumb from "@/layouts/Breadcrumb";
-import CountriesStatusCard from "../crm/components/CountriesStatusCard";
-import AudienceStatsCard from "./components/AudienceStatsCard";
-import PodcastEarningsOverviewCard from "./components/PodcastEarningsOverviewCard";
-import PodcastTopCategoriesCard from "./components/PodcastTopCategoriesCard";
-import RecentlyPlayedCard from "./components/RecentlyPlayedCard";
-import RecentPurposePlanCard from "./components/RecentPurposePlanCard";
-import TopPodcasterCard from "./components/TopPodcasterCard";
+// import CountriesStatusCard from "../crm/components/CountriesStatusCard";
+// import AudienceStatsCard from "./components/AudienceStatsCard";
+// import PodcastEarningsOverviewCard from "./components/PodcastEarningsOverviewCard";
+// import PodcastTopCategoriesCard from "./components/PodcastTopCategoriesCard";
+// import RecentlyPlayedCard from "./components/RecentlyPlayedCard";
+// import RecentPurposePlanCard from "./components/RecentPurposePlanCard";
+// import TopPodcasterCard from "./components/TopPodcasterCard";
 import TotalPodcasts from "./components/TotalPodcasts";
 import TotalUsers from "./components/TotalUsers";
-import TrendingEpisodesCard from "./components/TrendingEpisodesCard";
-// const TaskSummaryCard = lazy(() => import("./components/TaskSummaryCard"))
+// import TrendingEpisodesCard from "./components/TrendingEpisodesCard";
 
 const Podcast = () => {
     return (
@@ -18,7 +17,6 @@ const Podcast = () => {
             <Breadcrumb title="Podcast Desk" text="Podcast Desk" />
 
             <div className="grid grid-cols-1 md:grid-cols-12 gap-6">
-
                 <div className="col-span-12 lg:col-span-5 2xl:col-span-4">
                     <div className="grid grid-cols-1 md:grid-cols-12 gap-6">
                         <div className="col-span-12 md:col-span-6 lg:col-span-12">
@@ -35,56 +33,54 @@ const Podcast = () => {
                 </div>
 
                 <div className="col-span-12 lg:col-span-7 2xl:col-span-8">
-                    <LazyWrapper>
+                    {/* <LazyWrapper>
                         <AudienceStatsCard />
-                    </LazyWrapper>
+                    </LazyWrapper> */}
                 </div>
 
                 <div className="col-span-12 lg:col-span-4">
-                    <LazyWrapper>
+                    {/* <LazyWrapper>
                         <PodcastEarningsOverviewCard />
-                    </LazyWrapper>
+                    </LazyWrapper> */}
                 </div>
 
                 <div className="col-span-12 lg:col-span-8">
-                    <LazyWrapper>
+                    {/* <LazyWrapper>
                         <RecentlyPlayedCard />
-                    </LazyWrapper>
+                    </LazyWrapper> */}
                 </div>
 
                 <div className="col-span-12 2xl:col-span-8">
-                    <LazyWrapper>
+                    {/* <LazyWrapper>
                         <RecentPurposePlanCard />
-                    </LazyWrapper>
+                    </LazyWrapper> */}
                 </div>
 
                 <div className="col-span-12 md:col-span-6 2xl:col-span-4">
-                    <LazyWrapper>
+                    {/* <LazyWrapper>
                         <CountriesStatusCard />
-                    </LazyWrapper>
+                    </LazyWrapper> */}
                 </div>
 
                 <div className="col-span-12 md:col-span-6 2xl:col-span-4">
-                    <LazyWrapper>
+                    {/* <LazyWrapper>
                         <TrendingEpisodesCard />
-                    </LazyWrapper>
+                    </LazyWrapper> */}
                 </div>
 
                 <div className="col-span-12 md:col-span-6 2xl:col-span-4">
-                    <LazyWrapper>
+                    {/* <LazyWrapper>
                         <PodcastTopCategoriesCard />
-                    </LazyWrapper>
+                    </LazyWrapper> */}
                 </div>
 
                 <div className="col-span-12 md:col-span-6 2xl:col-span-4">
-                    <LazyWrapper>
+                    {/* <LazyWrapper>
                         <TopPodcasterCard />
-                    </LazyWrapper>
+                    </LazyWrapper> */}
                 </div>
 
             </div>
-
-
         </>
     );
 };
